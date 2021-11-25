75256b611c1f8bd9a331c780a8b35c3e9cd42165
ROHITCHANDRAN R <68961287+Rohitchandran242@users.noreply.github.com>
2021-11-25 19:31:53 +0530

    Update training.py

--- training.py
+++ training.py
@@ -39,16 +39,16 @@ def build model(args):
         model.summary(
         return model
 def train model(model, args, X_train, X_ valid, y_train, y_valid):
-  checkpoint = ModelCheckpoint('model-(epoch:03d}.h5',
-  monitor-'val loss',
-  verbose=0,save best only-args.save best only,mode='auto')
-  model.compile(loss-'mean_squared error',optimizer-Adam(Ir-args:.learning_rate))
-  model.,fit generator(batch generator(args.data dir, X train, y train,args.batch _size, True),args.samples _per epoch,args.nb_epoch,max q size=]
-validation data-batch generator(args .data dir, X valid, y _valid,
-args.batch _size, False),
-nb val samples=len(X valid),
-callbacks-[checkpoint],
-verbose=1)
+          checkpoint = ModelCheckpoint('model-(epoch:03d}.h5',
+          monitor-'val loss',
+          verbose=0,save best only-args.save best only,mode='auto')
+          model.compile(loss-'mean_squared error',optimizer-Adam(Ir-args:.learning_rate))
+          model.,fit generator(batch generator(args.data dir, X train, y train,args.batch _size, True),args.samples _per epoch,args.nb_epoch,max q size=]
+          validation data-batch generator(args .data dir, X valid, y _valid,
+          args.batch _size, False),
+          nb val samples=len(X valid),
+          callbacks-[checkpoint],
+          verbose=1)
 def s2b(s):
       #Converts a string to boolean value
       s=s.lower(
@@ -58,39 +58,38 @@ y' orS=
 = '1'
 def main:
 #Load train/validation data set and train the modelparserProgram')
-parser.add argument(-d"', help-datadefault='data')
-argparse.ArgumentParser(description-'Behavioral Cloning Training
-directory', dest='data dir',
-type-str,
-parser.add argument(-t', help-'test size fraction'.
-type=-Moat, default=0.2)
-dest='test size',
-parser.add argument(-k', help=drop out probability',
-type=float, default=0.5)
-dest='keep _prob',
-parser.add_argument(-n', help='number of epochs',
-type=int, default-10)
-parser.add _argument('-s',.
-dest='b epoch',
-help='samples
-dest='samples per epoch', type=int, default=20000)
-parser.add argument('-b', help= batch size',
-type=int, default=40)
-per
-epoch',
-dest='batch size',
-parser.add argument('-o', help='save best models only', dest-'save_best only,
-type=s2b, default='true')
-parser.add argument(-l', help-'learning rate',
-type=float, default=1.0€-4)
-dest=learning_rate',
-args = parser.parse args©
-print(- * 30)
-print('Parameters')
-print('-' * 30)
-for key, value in vars(args).items0:
-print('{:<20} := B'. format(key, value))
-print('-' * 30)
-data = load data(args)
-model = build model(args)
-train model(model, args, *data)
+                parser.add argument(-d"', help-datadefault='data')
+                argparse.ArgumentParser(description-'Behavioral Cloning Training
+                directory', dest='data dir',
+                type-str,
+                parser.add argument(-t', help-'test size fraction'.
+                type=-Moat, default=0.2)
+                dest='test size',
+                parser.add argument(-k', help=drop out probability',
+                type=float, default=0.5)
+                dest='keep _prob',
+                parser.add_argument(-n', help='number of epochs',
+                type=int, default-10)
+                parser.add _argument('-s',.
+                dest='b epoch',
+                help='samples
+                dest='samples per epoch', type=int, default=20000)
+                parser.add argument('-b', help= batch size',
+                type=int, default=40)
+                per
+                epoch',
+                dest='batch size',
+                parser.add argument('-o', help='save best models only', dest-'save_best only,
+                type=s2b, default='true')
+                parser.add argument(-l', help-'learning rate',type=float, default=1.0€-4)
+                dest=learning_rate',
+                args = parser.parse args©
+                print(- * 30)
+                print('Parameters')
+                print('-' * 30)
+                for key, value in vars(args).items0:
+                        print('{:<20} := B'. format(key, value))
+                        print('-' * 30)
+                        data = load data(args)
+                        model = build model(args)
+                        train model(model, args, *data)
