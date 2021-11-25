c13c9a4b54645214530b4d275ef3a8c5b0f27281
ROHITCHANDRAN R <68961287+Rohitchandran242@users.noreply.github.com>
2021-11-25 19:41:08 +0530

    Create drive.py

---
+++ drive.py
@@ -0,0 +1,108 @@
+import argparse
+import base64
+from datetime import datelime
+import os
+import shutil
+import numpy as mp
+import socketio
+import eventlet
+import eventlet.wsgi
+from PIL import Image
+from flask import Flask
+from io import BytesIO
+from keras.models import load model
+import utils
+sio = socketio.Server
+app= Flask( name
+model = None
+prev image array = None
+MAX SPEED = 30
+MIN SPEED = 10
+speed limit = MAX SPEED
+           @sio.on('telemetry")
+def telemetry(sid, data):
+            if data:
+                # The current steering angle of the car
+                steering angle = float(data["steering angle")
+                # The current throttle of the car
+                throttle = float(data("throttle"])
+                # The current speed of the car
+                speed = float( data["speed")
+                # The current image from the center camera of the car
+                image =Image.open(BytesIO(base64.b64decode(data["image"])))
+                # save frame
+            if args.image_folder!="
+                    timestamp
+                    datetime.utcnowO.strftime("%Y %m %d %H %M %S %P)[:-3]
+                    image_filename = os.path.join(args .image_folder, timestamp)
+                    image.save(*(} jpg' .format(image_filename))
+                    trv:
+                    image = np.asarray(image)
+                    # from PIL image to numpy array
+                    image = utils,preprocess(image) # apply the preprocessing
+                    image = np.array ([image])
+                    # the model expects 4D array
+                    # predict the steering angle for the image
+                    steering_angle = float(model,predict(image, batch size=1))
+                    # lower the throttle as the speed increases
+                    # if the speed is above the current speed limit, we are on a downhill.
+                    # make sure we slow down first and then go back to the original max
+                    speed.
+                    global speed limit
+            if speed > speed limit;
+                  speed limit - MIN SPEED # slow down
+            else:
+                speed limit - MAX SPEED
+                throttle =1.0 - steering angle*2 -(speed/speed limit)*2
+                print("© () G'. format(steering angle, throttle, speed))
+                send control(steering angle, throttle)
+                except Exception as e:
+                print(e)
+            else:
+                # NOTE: DON'T EDIT THIS.
+                sio.emit("manual', data=(;, skip sid-True)
+                @sio.on('connect')
+def connect(sid, environ):
+            print("connect ", sid)
+            send control(0, 0)
+def send control(steering_ angle, throttle):
+            sio.emit(
+            "steer".
+            data=!
+            'steering angle': steering angle.
+            str 0,
+            "throttle': throttle.
+            str (
+            skip_sid=True)
+              it
+            name
+            main
+            parser = argparse.ArgumentParser(description='Remote Driving')
+            parser.add argument(
+            'model',
+            type=str,
+            help-'Path to model h5 file. Model should be on the same path.'
+            parser.add argument
+            'image_folder
+            type=str,
+            nargs=?',
+            default="
+            help='Path to image folder. This is where the images from the run will be saved
+            arg = parser.parse_args(
+            model = load model (args.model)
+            if args.image folder !=":
+                  print("Creating image folder at ()". format(args.image_folder))
+            if not os.path.exists(args.image _folder):
+                  os.makedirs(args.image_folder)
+            else:
+                  shutil.rintree(args.image_folder)
+                  os.makedirs (args.image _folder)
+                  print("RECORDING THIS RUN ..
+                  . ")
+            else:
+                      print("NOT RECORDING THIS RUN .
+                )
+                # wrap Flask application with engineio's middleware
+                app = socketio.Middleware(sio, app)
+                # deploy as an eventlet WSGI server
+                eventlet.wsgi.server(eventlet.listen(", 4567)), app)
