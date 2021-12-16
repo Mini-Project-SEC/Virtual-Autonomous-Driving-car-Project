d0300b91572e242db7d6284541887a3fc3101429
ROHITCHANDRAN R <68961287+Rohitchandran242@users.noreply.github.com>
2021-12-16 08:11:31 +0530

    Update training.py

--- training.py
+++ training.py
@@ -10,86 +10,58 @@ from utils import INPUT SHAPE, batch generator
 import argparse
 import os
 np.random.seed(0)
+
 def load data(args):
         "Load training data and split it into training and validation set"
         data_df=pd.read_csv(os.path.join(args.data_dir, 'driving_log.csv'))
-        X= data dI['center', 'left', 'right']].values
-        y = data dfT'steering'] .values
-        X train,
-        X valid,
-        y train,
-        test_size-args.test size, random state=0)
-        y valid
-        train test split(X,
+        X= data_df[['center', 'left', 'right']].values
+        y = data df['steering'] .values
+        X_train, X_valid,y_train,y_valid=test_size-args.test_size, random_state=0
         return X_train, X valid, y train, y _valid
-def build model(args):
-        model = Sequential
-        model.add (Lambda (lambda »: ×/127.5-1.0, input shape=INPUT SHAPE))
-        model.add(Conv2D(24, 5, 5, activation='elu', subsample-(2, 2)
-        model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2))
-        model.add(Conv2D (48, 5, 5, activation='elu', subsample=(2, 2)))
-        model.add(Conv2D(64, 3, 3, activation=-elu))
-        model.add(Conv2D(64, 3, 3, activation='elu')
-        model.add(Dropout(args.keep_prob))
-        model.add (Flatten)
-        model.add(Dense(100, activation='elu'))
-        model.add(Dense(50, activation='elu'))
-        model.add(Dense(10, activation='elu'))
-        model.add(Dense(1))
-        model.summary(
-        return model
-def train model(model, args, X_train, X_ valid, y_train, y_valid):
-          checkpoint = ModelCheckpoint('model-(epoch:03d}.h5',
-          monitor-'val loss',
-          verbose=0,save best only-args.save best only,mode='auto')
-          model.compile(loss-'mean_squared error',optimizer-Adam(Ir-args:.learning_rate))
-          model.,fit generator(batch generator(args.data dir, X train, y train,args.batch _size, True),args.samples _per epoch,args.nb_epoch,max q size=]
-          validation data-batch generator(args .data dir, X valid, y _valid,
-          args.batch _size, False),
-          nb val samples=len(X valid),
-          callbacks-[checkpoint],
-          verbose=1)
-def s2b(s):
+        def build_model(args):
+                model = Sequential
+                model.add (Lambda (lambda x: x/127.5-1.0, input shape=INPUT SHAPE))
+                model.add(Conv2D(24, 5, 5, activation='elu', subsample-(2, 2)))
+                model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+                model.add(Conv2D (48, 5, 5, activation='elu', subsample=(2, 2)))                                 model.add(Conv2D(64, 3, 3, activation=-elu))
+                model.add(Conv2D(64, 3, 3, activation='elu')
+                model.add(Dropout(args.keep_prob))
+                model.add (Flatten)
+                model.add(Dense(100, activation='elu'))
+                model.add(Dense(50, activation='elu'))
+                model.add(Dense(10, activation='elu'))
+                model.add(Dense(1))
+                model.summary()
+                return model
+        def train model(model, args, X_train, X_ valid, y_train, y_valid):
+                checkpoint = ModelCheckpoint('model-(epoch:03d}.h5',monitor-'val loss',verbose=0,save best only-args.save best only,mode='auto')
+                model.compile(loss-'mean_squared error',optimizer-Adam(Ir-args:.learning_rate))
+                model.,fit generator(batch generator(args.data dir, X train, y train,args.batch _size, True),args.samples _per epoch,args.nb_epoch,max q size=]
+                validation data-batch generator(args .data dir, X valid, y _valid,
+                args.batch _size, False),
+                nb val samples=len(X valid),callbacks-[checkpoint],verbose=1)
+      def s2b(s):
       #Converts a string to boolean value
-      s=s.lower(
-return s == 'true' or S =
-= 'ves' or S =-
-y' orS=
-= '1'
-def main:
+      s=s.lower()
+        return s == 'true' or S == 'yes' or S ='y' or S== '1'
+        def main:
 #Load train/validation data set and train the modelparserProgram')
-                parser.add argument(-d"', help-datadefault='data')
-                argparse.ArgumentParser(description-'Behavioral Cloning Training
-                directory', dest='data dir',
-                type-str,
-                parser.add argument(-t', help-'test size fraction'.
-                type=-Moat, default=0.2)
-                dest='test size',
-                parser.add argument(-k', help=drop out probability',
-                type=float, default=0.5)
-                dest='keep _prob',
-                parser.add_argument(-n', help='number of epochs',
-                type=int, default-10)
-                parser.add _argument('-s',.
-                dest='b epoch',
-                help='samples
-                dest='samples per epoch', type=int, default=20000)
-                parser.add argument('-b', help= batch size',
-                type=int, default=40)
-                per
-                epoch',
+parser.add argument(-d"', help-datadefault='data')argparse.ArgumentParser(description-'Behavioral Cloning Trainingdirectory', dest='data dir',type-str,
+                parser.add argument('-t', help-'test size fraction'.type=-Moat, default=0.2)dest='test size',
+                parser.add argument('-k', help=drop out probability',type=float, default=0.5)dest='keep _prob',
+                parser.add_argument('-n', help='number of epochs',type=int, default-10)
+                parser.add _argument('-s',.dest='b epoch',help='samplesdest='samples per epoch', type=int, default=20000)
+                parser.add argument('-b', help= batch size',type=int, default=40)perepoch',
                 dest='batch size',
-                parser.add argument('-o', help='save best models only', dest-'save_best only,
-                type=s2b, default='true')
-                parser.add argument(-l', help-'learning rate',type=float, default=1.0€-4)
-                dest=learning_rate',
-                args = parser.parse args©
-                print(- * 30)
-                print('Parameters')
-                print('-' * 30)
-                for key, value in vars(args).items0:
-                        print('{:<20} := B'. format(key, value))
+                parser.add argument('-o', help='save best models only', dest-'save_best only,type=s2b, default='true')
+                parser.add argument('-l', help='learning rate',type=float, default=1.0€-4)
+                        dest=learning_rate',args = parser.parse args'
+                        print(- * 30)
+                        print('Parameters')
                         print('-' * 30)
-                        data = load data(args)
-                        model = build model(args)
-                        train model(model, args, *data)
+                        for key, value in vars(args).items0:
+                                print('{:<20} := B'. format(key, value))
+                                print('-' * 30)
+                                data = load data(args)
+                                model = build model(args)
+                                train model(model, args, *data)
